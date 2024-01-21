2926cb8827990237c31bdb2716e907dcb6950e91
Sebastián <126442441+zlcosio21@users.noreply.github.com>
2024-01-20 22:26:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let visibleMenu = false;
+
+function showOcultElements(){
+    if(visibleMenu){
+        document.getElementById("nav").classList ="";
+        visibleMenu = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        visibleMenu = true;
+    }
+}
+
+function select(){
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
+function efectSkills(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let skill = document.getElementsByClassName("progress");
+        skill[0].classList.add("htmlcss");
+        skill[1].classList.add("htmlcss");
+        skill[2].classList.add("photoshop");
+        skill[3].classList.add("wordpress");
+    }
+}
+
+
+window.onscroll = function(){
+    efectSkills();
+}
+
+function contentComplete(sectionId) {
+    var contentComplete = document.getElementById("contentComplete" + sectionId);
+    var buttonReadLess = document.querySelector(".see-more");
+
+    var computedStyle = window.getComputedStyle(contentComplete);
+
+    if (computedStyle.display === "none") {
+        contentComplete.style.display = "block";
+        buttonReadLess.innerHTML = "Read less";
+    } else {
+        contentComplete.style.display = "none";
+        buttonReadLess.innerHTML = "See more";
+    }
+}
